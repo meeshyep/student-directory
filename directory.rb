b35ba4193496752ba4b1afd3e92ab582eebcb605
Michelle Epstein <epstein.michelle@gmail.com>
2016-09-25 20:46:07 +0000

    added missing ) from the (november cohort) output

--- directory.rb
+++ directory.rb
@@ -25,7 +25,7 @@ end
 
 def print(students)
     students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort"
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end 
 end 
 
