5cbe18b5d152bc58cff84891ee9ae0120a9d768f
Michelle Epstein <epstein.michelle@gmail.com>
2016-09-25 16:49:11 +0000

    Student Directory - cleaned up with Arrays

--- directory.rb
+++ directory.rb
@@ -1,18 +1,15 @@
 #First we  print the list of students
 student_count = 11
+#Let's put all students into an array
+students = ["Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex Delarge", "The Wicked Witch of the West", "Terminator", "Freddy Krueger", "The Joker", "Norman Bates"
+]
+#and now print the student names
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#Then we put the total amount of awesome students
-print "Overall, we have #{student_count} great students"   
+students.each do |student|
+    puts student
+end 
+#Then we print the total number of awesome students
+print "Overall, we have #{students.count} great students\n"   
 #I did this three times: first time I wrote print for each line, second time I added an extra space before the quotation marks in line 16, third time's a charm
+# I also included a backslash and n on the ending code so that the sentence will end and cut to the next line
