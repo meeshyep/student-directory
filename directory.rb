dd3098241507ee7044fdf40cd645887786600710
Michelle Epstein <epstein.michelle@gmail.com>
2016-09-25 16:10:09 +0000

    Refactored my code to make it nice and clean!

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 #First we  print the list of students
+student_count = 11
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -13,7 +14,5 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 #Then we put the total amount of awesome students
-print "Overall we have "
-print 11
-puts " great students"   
+print "Overall, we have #{student_count} great students"   
 #I did this three times: first time I wrote print for each line, second time I added an extra space before the quotation marks in line 16, third time's a charm
